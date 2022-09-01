fa59dccc8f779a154ea70f21b8b3b171b5392f70
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-09-01 18:48:46 -0300

    feat: add hardskills container and sass

--- src/components/HardSkillsContainer.jsx
+++ src/components/HardSkillsContainer.jsx
@@ -1,15 +1,41 @@
+import '../styles/components/hardskills.sass';
+import {
+	DiHtml5,
+	DiCss3,
+	DiJsBadge,
+	DiNodejsSmall,
+	DiMysql,
+	DiReact,
+} from 'react-icons/di';
+
+const technologies = [
+	{ id: 'html', name: 'HTML5', icon: <DiHtml5 /> },
+	{ id: 'css', name: 'CSS3', icon: <DiCss3 /> },
+	{ id: 'js', name: 'JavaScript', icon: <DiJsBadge /> },
+	{ id: 'node', name: 'Node.js', icon: <DiNodejsSmall /> },
+	{ id: 'mysql', name: 'MySQL', icon: <DiMysql /> },
+	{ id: 'react', name: 'React', icon: <DiReact /> },
+];
+
 const HardSkills = () => {
 	return (
-		<div>
+		<section className="technologies-container">
 			<h2>Tecnologias</h2>
-			<p>
-				{' '}
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel
-				eius at amet neque, itaque tempore quo est alias. Dicta tempora
-				accusantium eius excepturi in voluptatum, et quas sequi libero
-				odio?{' '}
-			</p>
-		</div>
+			<div className="technologies-grid">
+				{technologies.map((tech) => (
+					<div className="technology-card" id={tech.id} key={tech.id}>
+						{tech.icon}
+						<div className="technology-info">
+							<h3>{tech.name}</h3>
+							<p>
+								Lorem ipsum dolor sit amet consectetur,
+								adipisicing elit.
+							</p>
+						</div>
+					</div>
+				))}
+			</div>
+		</section>
 	);
 };
 export default HardSkills;
